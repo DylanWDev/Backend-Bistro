b0e76978bcdebdebef7e0e5a46e4e24fcc9600a7
Dylan Wyrick <xpoweroutage@gmail.com>
2023-11-10 14:09:34 +0000

    join is done but not fully understood

--- routes/route.py
+++ routes/route.py
@@ -19,7 +19,11 @@ async def get_all_menu_items(db: Session = Depends(get_db)):
     result = []
     for item, category_name, cuisine_name in menu_items:
         result.append({
+            "id": item.id,
             "title": item.title,
+            "description": item.description,
+            "price": item.price,
+            "spicy_level": item.spicy_level,
             "category": category_name,
             "cuisine": cuisine_name
         })
